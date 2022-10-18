36de595d2a2ed56d9c2946fe383fac7a6daa1d04
Cdogo <john.tanaristy@gmail.com>
2022-10-18 17:09:38 -0500

    added countdown to home page and a place for form

--- components/page.js
+++ components/page.js
@@ -4,13 +4,13 @@ import Head from 'next/head'
 
 export default function Page({ children }) {
   return (
-    <div className={styles.page}>
+    <>
       <Head>
         <title>Centarui Hacks</title>
         <meta name="description" content="Centarui Hackathon 2022 a hackathon run by students"></meta>
       </Head>
       <Header />
       <div className={styles.sep}>{children}</div>
-    </div>
+    </>
   );
 }
